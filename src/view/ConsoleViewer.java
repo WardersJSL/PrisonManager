2e11e4832c52a63159ed158dbc0a130fb3733433
AncientGratin <ancientgratin@gmail.com>
2018-05-29 12:01:41 +0900

    Delete Viewer.java

--- src/view/ConsoleViewer.java
+++ src/view/ConsoleViewer.java
@@ -1,6 +1,10 @@
 package view;
 
+import controller.PrisonManager;
+
 //콘솔 UI 클래스
-public class ConsoleViewer extends Viewer {
+public class ConsoleViewer {
+	private PrisonManager prisonMgr;	// 교도소 관리자
+	
 	// TODO: 설계를 완료하고 나서 구현을 시작하겠습니다.
 }
