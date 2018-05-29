4bfaba58a87476a3ff67fd7e3c8812c525a607ce
hyeon3713 <hyeon3713@naver.com>
2018-05-29 12:44:40 +0900

    prectice1

--- src/controller/PrisonManager.java
+++ src/controller/PrisonManager.java
@@ -5,8 +5,23 @@ import java.util.ArrayList;
 import model.Prisoner;
 
 //교도소 관리 클래스(back-end)
+//유효성검사
 public class PrisonManager {
-	private ArrayList<Prisoner> prisoners;		// 죄수들
+	private ArrayList<Prisoner> ImsiPrisoners = new ArrayList<Prisoner>();		// 죄수들
 	
-	// TODO: 설계를 완료하고 나서 구현을 시작하겠습니다.
+	public void PrisonManager(){
+		
+	}
+	public void AddPrisoner() {			//죄수등록
+		
+	}
+	public void DeletePrisoner() {		//죄수삭제
+		
+	}
+	public void FindPrisoner() {		//죄수검색
+
+	}
+	public void ShowAll() {				//죄수전체출력
+		
+	}
 }
