bf2b4bfb3d722046cfe4b7fd85a6ccf69c0a0af3
AncientGratin <ancientgratin@gmail.com>
2018-05-30 09:35:21 +0900

    Modify MainProcessor

--- src/main/console/MainProcessor.java
+++ src/main/console/MainProcessor.java
@@ -6,7 +6,28 @@ public class MainProcessor {
 	
 	public static void main(String[] args) {
 		ConsoleViewer console = new ConsoleViewer();	// 콘솔 UI 관리자
+		int menu = 0;
 		
-		System.out.println("Hello prison!");
+		while(true) {
+			// 메뉴 출력
+			// 사용자에게 메뉴 번호를 받아 변수 menu에 저장
+			switch(menu) {
+			case 1:
+				// 입소메뉴 실행
+				break;
+			case 2:
+				// 출소메뉴 실행
+				break;
+			case 3:
+				// 검색메뉴 실행
+				break;
+			case 4:
+				// 전체조회 실행
+				break;
+			case 5:
+				// 프로그램 종료
+				System.exit(0);
+			}
+		}
 	}
 }
--- src/view/ConsoleViewer.java
+++ src/view/ConsoleViewer.java
@@ -1,10 +1,10 @@
 package view;
 
-import controller.PrisonManager;
+import controller.PrisonController;
 
 //콘솔 UI 클래스
 public class ConsoleViewer {
-	private PrisonManager prisonMgr;	// 교도소 관리자
+	private PrisonController prisonMgr;	// 교도소 관리자
 	
 	// TODO: 설계를 완료하고 나서 구현을 시작하겠습니다.
 }
