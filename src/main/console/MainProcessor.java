0e1c896739616719ed45078e7797e0e61ac49464
AncientGratin <ancientgratin@gmail.com>
2018-06-01 12:36:51 +0900

    Put out codes of MainProcessor from comment

--- src/main/console/MainProcessor.java
+++ src/main/console/MainProcessor.java
@@ -8,32 +8,32 @@ public class MainProcessor {
 		ConsoleViewer console = new ConsoleViewer();	// 콘솔 UI 관리자
 		int menu = 0;
 		
-//		while(true) {
-//			// 메뉴 출력
-//			console.showMenu();
-//			
-//			// 사용자에게 메뉴 번호를 받아 변수 menu에 저장
-//			switch(menu) {
-//			case 1:
-//				// 입소메뉴 실행
-//				console.enter();
-//				break;
-//			case 2:
-//				// 출소메뉴 실행
-//				console.release();
-//				break;
-//			case 3:
-//				// 검색메뉴 실행
-//				console.search();
-//				break;
-//			case 4:
-//				// 전체조회 실행
-//				console.showAll();
-//				break;
-//			case 5:
-//				// 프로그램 종료
-//				System.exit(0);
-//			}
-//		}
+		while(true) {
+			// 메뉴 출력
+			console.showMenu();
+			
+			// 사용자에게 메뉴 번호를 받아 변수 menu에 저장
+			switch(console.menuCheck()) {
+			case 1:
+				// 입소메뉴 실행
+				console.enter();
+				break;
+			case 2:
+				// 출소메뉴 실행
+				console.release();
+				break;
+			case 3:
+				// 검색메뉴 실행
+				console.search();
+				break;
+			case 4:
+				// 전체조회 실행
+				console.showAll();
+				break;
+			case 5:
+				// 프로그램 종료
+				System.exit(0);
+			}
+		}
 	}
 }
